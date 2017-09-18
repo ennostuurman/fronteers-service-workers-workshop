fef17b1105f43c6a0ac03aeb52801c857cb698d7
Enno Stuurman <enno@spaarnewebdesign.nl>
2017-09-18 12:42:36 +0200

    Add offline cache solution

--- views/sw.js
+++ views/sw.js
@@ -1,17 +1,6 @@
 // import content from '../data/content.json'
 import { isSWFormSync, doSWFormSync } from './components/form/form.js'
 
-const CACHE_VERSION = 'v1';
-const RESOURCES_TO_CACHE = [
-	'/fronteers.css',
-	'/index.js',
-	'/logo.png',
-	'/bullet.png',
-	'/workshops',
-	'/workshops/service-workers',
-	'/workshops/service-workers/15-september-2017'
-]
-
 console.log(`Hi! I'm a service worker from ${self.origin}`)
 
 //
@@ -19,12 +8,6 @@ console.log(`Hi! I'm a service worker from ${self.origin}`)
 //
 self.addEventListener('install', event => {
 	console.log('sw install', event)
-	event.waitUntil(
-		caches.open(CACHE_VERSION).then(cache => {
-			console.log('caching...')
-			cache.addAll(RESOURCES_TO_CACHE)
-		}).then(self.skipWaiting())
-	)
 })
 
 self.addEventListener('activate', event => {
@@ -36,26 +19,10 @@ self.addEventListener('activate', event => {
 //
 self.addEventListener('fetch', event => {
 	console.log('user agent fetch', event.request, event)
-	event.respondWith(
-		caches.match(event.request)
-			.then(response => {
-				return response || fetch(event.request)
-			})
-	)
 })
 
 self.addEventListener('sync', event => {
 	console.log('user agent sync', event)
-	if (isSWFormSync(event)) {
-		event.waitUntil(
-			doSWFormSync(event)
-				.then(() => console.log('sync succeeded'))
-				.catch(() => {
-					console.log('retry sync')
-					return Promise.reject(event)
-				})
-		)
-	}
 })
 
 self.addEventListener('push', event => {
